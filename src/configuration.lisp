b7ecdadc730afeb0847378bf8c025e8c2a549e86
Lukáš Hozda <luk.hozda@gmail.com>
2026-07-25 18:23:15 +0200

    Release Autolith 0.16.3

--- src/configuration.lisp
+++ src/configuration.lisp
@@ -2,7 +2,7 @@
 
 ;;;; -- Defaults --
 
-(defparameter *autolith-version* "0.16.2"
+(defparameter *autolith-version* "0.16.3"
   "The user-visible Autolith version.")
 
 (defparameter *default-model* "gpt-5.6-sol"
