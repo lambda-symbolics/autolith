3a9640989be71175ce5b7a55a1e59c5780db52c8
Lukáš Hozda <luk.hozda@gmail.com>
2026-09-10 16:09:26 +0200

    Use checkout path for Windows Quicklisp bootstrap

--- script/ensure-quicklisp.lisp
+++ script/ensure-quicklisp.lisp
@@ -1,11 +1,17 @@
 (require :asdf)
 (let ((setup (merge-pathnames "quicklisp/setup.lisp" (user-homedir-pathname))))
   (unless (probe-file setup)
-    (let ((installer (merge-pathnames "quicklisp.lisp" (uiop:temporary-directory))))
-      (uiop:run-program (list "curl.exe" "--fail" "--location" "--show-error" "--retry" "3"
-                              "--output" (uiop:native-namestring installer)
-                              "https://beta.quicklisp.org/quicklisp.lisp")
-      (load installer)
-      (funcall (find-symbol "INSTALL" "QUICKLISP-QUICKSTART")
-               :path (merge-pathnames "quicklisp/" (user-homedir-pathname))))))
-)
+    (let ((installer (merge-pathnames ".quicklisp-installer.lisp"
+                                      (uiop:pathname-directory-pathname
+                                       (truename *load-truename*)))))
+      (unwind-protect
+           (progn
+             (uiop:run-program
+              (list "curl.exe" "--fail" "--location" "--show-error" "--retry" "3"
+                    "--output" (uiop:native-namestring installer)
+                    "https://beta.quicklisp.org/quicklisp.lisp")
+             (load installer)
+             (funcall (find-symbol "INSTALL" "QUICKLISP-QUICKSTART")
+                      :path (merge-pathnames "quicklisp/" (user-homedir-pathname))))
+        (when (probe-file installer)
+          (delete-file installer))))))
